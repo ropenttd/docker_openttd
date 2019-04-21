b3792c4854f4f64f6ee52ea401dca4510832c498
Thomas Hahn <Thahn@tcbtech.com>
2019-04-20 20:29:25 -0700

    Complete rewrite to duplicate less, and handle pod failure better. Exit strategy should use last-autosave if last-autosave is newer than exit savefile. Otherwirse, load the savefile if present, and if not present, create a new game.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-# This script is based fairly heavily off bateau84/openttd's. Thanks, man!
+# This script is based off bateau84/openttd's. Thanks, man!
 savepath="/config/save"
-LOADGAME_CHECK="${loadgame:=autosave/exit.sav}x"
 
 if [ ! -f /config/openttd.cfg ]; then
   # we start the server then kill it quickly to write a config file
@@ -11,46 +10,31 @@ if [ ! -f /config/openttd.cfg ]; then
   timeout -t 3 /app/bin/openttd -D > /dev/null 2>&1
 fi
 
-if [ "${LOADGAME_CHECK}" != "x" ]; then
-  case ${loadgame} in
-    'false')
+autosave_target=""
+savegame_target="${savepath}/${loadgame:=exit}"
+case ${loadgame} in
+  'exit')
+    savegame_target="${savepath}/autosave/exit.sav"
+    ;&
+  'last-autosave')
+    autosave_target="${savepath}/autosave/$(ls -1rt ${savepath}/autosave/ | tail -n1)"
+    # IFF the the exit savegame is older than the autosave, we're recovering from a crash.
+    # Also handles the not-found case - A nonexistent file is older than an existing file.
+    if [[ "${savegame_target}" -ot "${autosave_target}" ]]; then
+      savegame_target="${autosave_target}"
+    fi
+    ;;&
+  *)
+    if [ -f "${savegame_target}" ]; then
+      echo "Loading ${savegame_target}"
+      exec /app/bin/openttd -D -g "${savegame_target}" -x -d "${DEBUG}"
+    else
+      if [[ ${loadgame} != "false" ]]; then
+        echo "${savegame_target} not found..."
+      fi
       echo "Creating a new game."
       exec /app/bin/openttd -D -x -d "${DEBUG}"
-      ;;
-    'last-autosave')
-      savegame_target="${savepath}/autosave/$(ls -1rt ${savepath}/autosave/ | tail -n1)"
-
-      if [ -r "${savegame_target}" ]; then
-        echo "Loading from autosave - ${savegame_target}"
-        exec /app/bin/openttd -D -g "${savegame_target}" -x -d "${DEBUG}"
-      else
-        echo "${savegame_target} not found..."
-        exit 1
-      fi
-      ;;
-    'exit')
-      savegame_target="${savepath}/autosave/exit.sav"
-
-      if [ -r "${savegame_target}" ]; then
-        echo "Loading from exit save"
-        exec /app/bin/openttd -D -g "${savegame_target}" -x -d "${DEBUG}"
-      else
-        echo "${savegame_target} not found - Creating a new game."
-        exec /app/bin/openttd -D -x -d "${DEBUG}"
-      fi
-      ;;
-    *)
-      savegame_target="${savepath}/${loadgame}"
-      if [ -f "${savegame_target}" ]; then
-        echo "Loading ${savegame_target}"
-        exec /app/bin/openttd -D -g "${savegame_target}" -x -d "${DEBUG}"
-      else
-        echo "${savegame_target} not found..."
-        exit 1
-      fi
-      ;;
-  esac
-else
-  echo "loadgame not set - Creating a new game."
-  exec /app/bin/openttd -D -x -d "${DEBUG}"
-fi
+      exit 1
+    fi
+    ;;
+esac
