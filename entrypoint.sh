7f62e11cd00cd3a27cf9a00c4a22122ebd7eb2b0
duck. <me@duck.me.uk>
2025-01-09 16:32:37 +0000

    2025.1.2: Improvements to logic for loading save files - #23
    Should properly escape and handle save file names provided on the `loadgame` parameter now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # This script is based fairly heavily off bateau84/openttd's. Thanks, man!
-savepath="/config/save"
+
+SAVEPATH="/config/save"
 LOADGAME_CHECK="${loadgame}x"
 EXTRA_FLAGS="-c /config/openttd.cfg"
 
 # Required to force config to save to /config
 if [ -f /config/.config/openttd.cfg ]; then
         export XDG_DATA_HOME=''
+        SAVEPATH="/config/.config/save"
         EXTRA_FLAGS="-c /config/.config/openttd.cfg"
         echo "WARN: Using legacy configuration directory /config/.config/ - it is recommended to migrate to all data inside /config/* when possible."
 elif [ ! -f /config/openttd.cfg ]; then
@@ -17,48 +19,48 @@ elif [ ! -f /config/openttd.cfg ]; then
         timeout 3 /app/openttd -D ${EXTRA_FLAGS} > /dev/null 2>&1
 fi
 
-if [ ${LOADGAME_CHECK} != "x" ]; then
+if [ "${LOADGAME_CHECK}" != "x" ]; then
         case ${loadgame} in
                 'false')
-                	echo "INFO: Creating a new game."
-                    exec /app/openttd -D ${EXTRA_FLAGS} -x  -d ${DEBUG}
-                    exit 0
+                        echo "INFO: Creating a new game."
+                        exec /app/openttd -D ${EXTRA_FLAGS} -x  -d ${DEBUG}
+                        exit 0
                 ;;
                 'last-autosave')
-            		savegame_target=${savepath}/autosave/`ls -rt ${savepath}/autosave/ | tail -n1`
+            		SAVEGAME_TARGET=`ls -rt ${SAVEPATH}/autosave/*.sav | tail -n1`
 
-            		if [ -r ${savegame_target} ]; then
-                    	echo "INFO: Loading from autosave - ${savegame_target}"
-                        exec /app/openttd -D ${EXTRA_FLAGS} -g ${savegame_target} -x -d ${DEBUG}
+            		if [ -r "${SAVEGAME_TARGET}" ]; then
+                    	echo "INFO: Loading from latest autosave - ${SAVEGAME_TARGET}"
+                        exec /app/openttd -D ${EXTRA_FLAGS} -g ${SAVEGAME_TARGET} -x -d ${DEBUG}
                         exit 0
             		else
-                		echo "${savegame_target} not found..."
+                		echo "FATAL: ${SAVEGAME_TARGET} not found"
                 		exit 1
             		fi
                 ;;
                 'exit')
-            		savegame_target="${savepath}/autosave/exit.sav"
+            		SAVEGAME_TARGET="${SAVEPATH}/autosave/exit.sav"
 
-            		if [ -r ${savegame_target} ]; then
+            		if [ -r "${SAVEGAME_TARGET}" ]; then
                     	echo "INFO: Loading from exit save"
-                        exec /app/openttd -D ${EXTRA_FLAGS} -g ${savegame_target} -x -d ${DEBUG}
+                        exec /app/openttd -D ${EXTRA_FLAGS} -g "${SAVEGAME_TARGET}" -x -d ${DEBUG}
                         exit 0
             		else
-                		echo "${savegame_target} not found - Creating a new game."
+                		echo "${SAVEGAME_TARGET} not found - Creating a new game."
                 		exec /app/openttd -D ${EXTRA_FLAGS} -x -d ${DEBUG}
                     	exit 0
             		fi
                 ;;
                 *)
-                	savegame_target="${savepath}/${loadgame}"
-                    if [ -f ${savegame_target} ]; then
-                            echo "INFO: Loading ${savegame_target}"
-                            exec /app/openttd -D ${EXTRA_FLAGS} -g ${savegame_target} -x -d ${DEBUG}
-                            exit 0
-                    else
-                            echo "${savegame_target} not found..."
-                            exit 1
-                    fi
+                        SAVEGAME_TARGET="${SAVEPATH}/${loadgame}"
+                        if [ -r "${SAVEGAME_TARGET}" ]; then
+                                echo "INFO: Loading ${SAVEGAME_TARGET}"
+                                exec /app/openttd -D ${EXTRA_FLAGS} -g "${SAVEGAME_TARGET}" -x -d ${DEBUG}
+                                exit 0
+                        else
+                                echo "FATAL: ${SAVEGAME_TARGET} not found"
+                                exit 1
+                        fi
                 ;;
         esac
 else
